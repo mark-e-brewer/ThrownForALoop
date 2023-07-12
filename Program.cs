c3c3b375d7aa4c04d2276a38c58d19edbe78ae33
Mark Brewer <markbrewer369@gmail.com>
2023-07-11 21:35:30 -0500

    conditional testing

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿string greeting = @"Welcome to Thrown For a Loop
+Your one-stop shop for used sporting equipment";
+
+Console.WriteLine(greeting);
+
+Console.WriteLine("Please enter a product name: ");
+
+string response = Console.ReadLine();
+
+if (string.IsNullOrWhiteSpace(response))
+{
+    Console.WriteLine("You didn't choose anything!");
+}
+else
+{
+    Console.WriteLine($"You chose: {response}");
+}
